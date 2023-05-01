d7451ec332571e3dba316336d3a09d2897ecd6e9
charlie <charlie@deltr.io>
2023-05-01 21:19:03 +0000

    updated

--- src/index.php
+++ src/index.php
@@ -1,16 +1,372 @@
-<?php // Personal website ?>
-
+<!DOCTYPE html>
 <html>
 
 <head>
-    <title>charlie</title>
-    <meta charset="utf-8">
-    <meta property="og:title" content="charlie">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv='content-type' content='text/html;charset=utf8'>
+    <title>charlie_maddex(1) - the future is radiant</title>
+    <link rel="icon" type="image/png" href="https://avatars.githubusercontent.com/u/39627038?v=4">
+    <style type='text/css' media='all'>
+    /* style: man */
+    body#manpage {
+        margin: 0
+    }
+
+    .mp {
+        max-width: 100ex;
+        padding: 0 9ex 1ex 4ex
+    }
+
+    .mp p,
+    .mp pre,
+    .mp ul,
+    .mp ol,
+    .mp dl {
+        margin: 0 0 20px 0
+    }
+
+    .mp h2 {
+        margin: 10px 0 0 0
+    }
+
+    .mp>p,
+    .mp>pre,
+    .mp>ul,
+    .mp>ol,
+    .mp>dl {
+        margin-left: 8ex
+    }
+
+    .mp h3 {
+        margin: 0 0 0 4ex
+    }
+
+    .mp dt {
+        margin: 0;
+        clear: left
+    }
+
+    .mp dt.flush {
+        float: left;
+        width: 8ex
+    }
+
+    .mp dd {
+        margin: 0 0 0 9ex
+    }
+
+    .mp h1,
+    .mp h2,
+    .mp h3,
+    .mp h4 {
+        clear: left
+    }
+
+    .mp pre {
+        margin-bottom: 20px
+    }
+
+    .mp pre+h2,
+    .mp pre+h3 {
+        margin-top: 22px
+    }
+
+    .mp h2+pre,
+    .mp h3+pre {
+        margin-top: 5px
+    }
+
+    .mp img {
+        display: block;
+        margin: auto
+    }
+
+    .mp h1.man-title {
+        display: none
+    }
+
+    .mp,
+    .mp code,
+    .mp pre,
+    .mp tt,
+    .mp kbd,
+    .mp samp,
+    .mp h3,
+    .mp h4 {
+        font-family: monospace;
+        font-size: 14px;
+        line-height: 1.42857142857143
+    }
+
+    .mp h2 {
+        font-size: 16px;
+        line-height: 1.25
+    }
+
+    .mp h1 {
+        font-size: 20px;
+        line-height: 2
+    }
+
+    .mp {
+        text-align: justify;
+        background: #fff
+    }
+
+    .mp,
+    .mp code,
+    .mp pre,
+    .mp pre code,
+    .mp tt,
+    .mp kbd,
+    .mp samp {
+        color: #131211
+    }
+
+    .mp h1,
+    .mp h2,
+    .mp h3,
+    .mp h4 {
+        color: #030201
+    }
+
+    .mp u {
+        text-decoration: underline
+    }
+
+    .mp code,
+    .mp strong,
+    .mp b {
+        font-weight: bold;
+        color: #131211
+    }
+
+    .mp em,
+    .mp var {
+        font-style: italic;
+        color: #232221;
+        text-decoration: none
+    }
+
+    .mp a,
+    .mp a:link,
+    .mp a:hover,
+    .mp a code,
+    .mp a pre,
+    .mp a tt,
+    .mp a kbd,
+    .mp a samp {
+        color: #0000ff
+    }
+
+    .mp b.man-ref {
+        font-weight: normal;
+        color: #434241
+    }
+
+    .mp pre {
+        padding: 0 4ex
+    }
+
+    .mp pre code {
+        font-weight: normal;
+        color: #434241
+    }
+
+    .mp h2+pre,
+    h3+pre {
+        padding-left: 0
+    }
+
+    ol.man-decor,
+    ol.man-decor li {
+        margin: 3px 0 10px 0;
+        padding: 0;
+        float: left;
+        width: 33%;
+        list-style-type: none;
+        text-transform: uppercase;
+        color: #999;
+        letter-spacing: 1px
+    }
+
+    ol.man-decor {
+        width: 100%
+    }
+
+    ol.man-decor li.tl {
+        text-align: left
+    }
+
+    ol.man-decor li.tc {
+        text-align: center;
+        letter-spacing: 4px
+    }
+
+    ol.man-decor li.tr {
+        text-align: right;
+        float: right
+    }
+    </style>
+    <style type='text/css' media='all'>
+    /* style: toc */
+    .man-navigation {
+        display: block !important;
+        position: fixed;
+        top: 0;
+        left: 113ex;
+        height: 100%;
+        width: 100%;
+        padding: 48px 0 0 0;
+        border-left: 1px solid #dbdbdb;
+        background: #eee
+    }
+
+    .man-navigation a,
+    .man-navigation a:hover,
+    .man-navigation a:link,
+    .man-navigation a:visited {
+        display: block;
+        margin: 0;
+        padding: 5px 2px 5px 30px;
+        color: #999;
+        text-decoration: none
+    }
+
+    .man-navigation a:hover {
+        color: #111;
+        text-decoration: underline
+    }
+    </style>
 </head>
 
-<body>
+<body id='manpage'>
+    <div class='mp' id='man'>
 
+        <div class='man-navigation' style='display:none'>
+            <a href="#NAME">NAME</a>
+            <a href="#SYNOPSIS">SYNOPSIS</a>
+            <a href="#DESCRIPTION">DESCRIPTION</a>
+            <a href="#HISTORY">HISTORY</a>
+            <a href="#SEE-ALSO">SEE ALSO</a>
+        </div>
+
+        <ol class='man-decor man-head man head'>
+            <li class='tl'>charlie_maddex(1)</li>
+            <li class='tc'></li>
+            <li class='tr'>charlie_maddex(1)</li>
+        </ol>
+
+
+
+        <h2 id="NAME">NAME</h2>
+        <p class="man-name">
+            <code>charlie_maddex</code> - <span class="man-whatis">the future is radiant</span>
+        </p>
+        <h2 id="SYNOPSIS">SYNOPSIS</h2>
+
+        <p><code>charlie</code> <code>--description</code><br>
+            <code>charlie</code> <code>--history</code><br>
+            <code>charlie</code> <code>--see_also</code><br>
+        </p>
+
+        <h2 id="DESCRIPTION">DESCRIPTION</h2>
+
+        <p>charlie is a software engineer who enjoys infrastructure and systems administration, cryptography, computer
+            security, game anti-cheat, and networking. He enjoys connecting people with technology solutions that are
+            easy to use, affordable, and sustainable over time. He is familiar with, and a huge supporter of, the DevOps
+            culture and how to make it work with a small team in a fast paced environment.
+            He currently works at Method-IT, a Managed Service Provider who provides managed cloud and communication
+            systems. He also works on Anglo, a artificial general intelligence research group in his free time.
+        </p>
+
+        <h2 id="HISTORY">HISTORY</h2>
+
+        <ul>
+            <li>
+                <p>Method-IT <em>2019-Present</em></p>
+
+                <ul>
+                    <li>Systems Administrator</li>
+                    <li>Automation</li>
+                    <li>Patch Management</li>
+                    <li>System Monitoring</li>
+                    <li>Infrastructure</li>
+                    <li>Cloud Computing</li>
+                    <li>Security</li>
+                </ul>
+            </li>
+            <li>
+                <p>Anglo <em>2022-Present</em></p>
+
+                <ul>
+                    <li>Infrastructure</li>
+                    <li>Network Engineering</li>
+                    <li>Cloud Computing</li>
+                    <li>Database Management</li>
+                    <li>Security</li>
+                    <li>DevOps</li>
+                </ul>
+            </li>
+            <li>
+                <p>Deltr <em>2022-2023</em></p>
+
+                <ul>
+                    <li>Lead Developer</li>
+                    <li>Infrastructure</li>
+                    <li>Cloud Computing</li>
+                    <li>Database Management</li>
+                    <li>Security</li>
+                    <li>DevOps</li>
+                </ul>
+            </li>
+            <li>
+                <p>Southend University Hospital NHS <em>2018-2019</em></p>
+
+                <ul>
+                    <li>Help Desk Technician</li>
+                    <li>Active Directory</li>
+                    <li>Windows Server</li>
+                    <li>Office 365</li>
+                    <li>Hardware Support</li>
+                    <li>Software Support</li>
+                    <li>Network Support</li>
+                </ul>
+            </li>
+            <li>
+                <p>Ofcom <em>2015-Present</em></p>
+
+                <ul>
+                    <li>Amateur Intermediate Radio Licence</li>
+                </ul>
+            </li>
+            <li>
+                <p>Ofcom <em>2011-Present</em></p>
+
+                <ul>
+                    <li>Amateur Foundation Radio Licence</li>
+                </ul>
+            </li>
+        </ul>
+
+        <h2 id="SEE-ALSO">SEE ALSO</h2>
+
+        <ul>
+            <li>GitHub: <a href="https://github.com/name">name</a>
+            </li>
+            <li>Email: <a href="mailto:charlie@anglo.is" data-bare-link="true">charlie@anglo.is</a>
+            </li>
+            <li>LinkedIn: <a href="https://www.linkedin.com/in/charliemaddex/">charliemaddex</a>
+            </li>
+            <li>Twitter: <a href="https://twitter.com/charliemaddex">@charliemaddex</a>
+        </ul>
+
+        <ol class='man-decor man-foot man foot'>
+            <li class='tl'></li>
+            <li class='tc'>May 2023</li>
+            <li class='tr'>charlie_maddex(1)</li>
+        </ol>
+
+    </div>
 </body>
 
 </html>
