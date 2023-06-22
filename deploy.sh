7c01e9a017ceb0df9fdc8063c3f5aa5318911241
charlie <charlie@anglo.is>
2023-06-22 23:02:27 +0100

    updated code comments

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
-echo "Deploying personal website 'maddex.co'  using Docker Compose"
+# Start the deployment to Docker
+echo "Deploying personal website 'maddex.co' using Docker Compose"
 
-# Run Docker Compose to deploy Portainer from scratch
+# Run Docker Compose with --build to compile from scratch
 docker compose up -d --build
 
 # Tidy up any unused Docker images
 docker image prune -f
 
+# Completed deployment
 echo "Deployed successfully"
