aa2f25e0c4112f1b2f3227e899d142b17697c7c0
charlie <charlie@anglo.is>
2023-06-25 20:07:07 +0000

    update

--- src/index.php
+++ src/index.php
@@ -2,34 +2,73 @@
 <html lang="en">
 
 <head>
-	<meta charSet="utf-8"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<meta http-equiv="X-UA-Compatible" content="ie-edge"/>
-	<link rel="icon" href="/images/av32x32.png"/>
-	<link rel="apple-touch-icon" href="/images/av256x256.png"/>
-	<meta name="robots" content="index,follow"/>
-	<title>Charlie Maddex</title>
-	<meta name="description" content="Charlie Maddex's site"/>
-	<meta name="theme-color" content="#ffffff"/>
-	<meta property="og:title" content="Charlie Maddex"/>
-	<meta property="og:description" content="Charlie Maddex's site."/>
-	<meta property="og:url" content="https://maddex.co"/>
-	<meta name="twitter:image" content="https://maddex.co/images/banner.jpg"/>
-	<meta property="og:type" content="website"/>
-	<meta property="og:locale" content="en_IE"/>
-	<meta property="og:site_name" content="Charlie Maddex"/>
-	<meta property="og:image:alt" content="og image"/>
-	<meta name="twitter:card" content="summary_large_image"/>
-	<meta name="twitter:site" content="@cunjur"/>
-	<meta name="twitter:creator" content="@cunjur"/>
-	<meta name="twitter:title" content="no e-mail"/>
-	<meta name="twitter:description" content="charlie's journal"/>
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie-edge" />
+    <link rel="icon" href="/images/av32x32.png" />
+    <link rel="apple-touch-icon" href="/images/av256x256.png" />
+    <meta name="robots" content="index,follow" />
+    <title>Charlie Maddex</title>
+    <meta name="description" content="Charlie Maddex's site" />
+    <meta name="theme-color" content="#ffffff" />
+    <meta property="og:title" content="Charlie Maddex" />
+    <meta property="og:description" content="Charlie Maddex's site." />
+    <meta property="og:url" content="https://maddex.co" />
+    <meta name="twitter:image" content="https://maddex.co/images/banner.jpg" />
+    <meta property="og:type" content="website" />
+    <meta property="og:locale" content="en_IE" />
+    <meta property="og:site_name" content="Charlie Maddex" />
+    <meta property="og:image:alt" content="og image" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@cunjur" />
+    <meta name="twitter:creator" content="@cunjur" />
+    <meta name="twitter:title" content="no e-mail" />
+    <meta name="twitter:description" content="charlie's journal" />
+    <style>
+    * {
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+    }
+
+    body,
+    html {
+        max-width: 100vw;
+        overflow-x: hidden;
+        font-antialiasing: antialiased;
+        padding: .5rem;
+    }
+
+    section {
+        display: block;
+    }
+
+    li {
+        display: list-item;
+        text-align: -webkit-match-parent;
+    }
+    </style>
 </head>
 
 <body>
-<div>
-	<main>
-		Charlie Maddex. 21, Senior Technical Analyst, Full-Stack Dev & Network Engineer, and Elixir Advocate. I'm
+    <div>
+        <main>
+
+            <p style="margin-bottom:1em;font-weight:800">notes</p>
+            <?php
+				// Get the contents of a website
+				$homepage = file_get_contents('https://maddex.co/notes');
+			?>
+        </main>
+    </div>
+</body>
+
+</html>
+
+<?php
+/*
+
+Charlie Maddex. 21, Senior Technical Analyst, Full-Stack Dev & Network Engineer, and Elixir Advocate. I'm
 		mostly placing my energy in <a rel="noopener noreferrer" target="_blank" style="margin-left:2px;color:blue"
 									   href="https://anglo.is/">Anglo</a>, but I'm starting to work on small open-source
 		projects for my
@@ -52,8 +91,6 @@
 		If you are contacting me regarding a sensitive issue, please encrypt your communications using my <a
 			style="margin-left:2px;color:blue" target="_blank" href="/pgp">pgp
 			key.</a>.
-	</main>
-</div>
-</body>
 
-</html>
+*/
+?>
